5d4fc8cd6e62f865cf5bff046afe56f3bc15d17c
rogitson <omarm.elaraby2000@gmail.com>
2022-05-24 17:53:55 +0100

    implemented more basic functions

--- simple-shell.c
+++ simple-shell.c
@@ -6,13 +6,43 @@
 
 #define MAXLEN 1024
 #define MAXARGS 256
+#define true 1
+#define false !true
 
-void execute(char** args)
+pid_t pid;
+char* currentDirectory;
+
+void init()
 {
-    if(strcmp(args[0], "exit") == 0) exit(0);
-    pid_t pid;
+    // Get the current directory that will be used in different methods
+	currentDirectory = (char*) calloc(1024, sizeof(char));
+}
+
+void prompt(){
+	// We print the prompt in the form "<user>@<host> <cwd> >"
+	char hostn[1204] = "";
+	gethostname(hostn, sizeof(hostn));
+	printf("%s@%s %s > ", getenv("LOGNAME"), hostn, getcwd(currentDirectory, 1024));
+}
+
+
+void cd(char** args)
+{
+    // If we write no path (only 'cd'), then go to the home directory
+	if (args[1] == NULL) chdir(getenv("HOME")); 
+	// Else we change the directory to the one specified by the argument, if possible
+	else if (chdir(args[1]) == -1) printf("%s: no such directory\n", args[1]);
+	return;
+}
+
+void launch(char** args)
+{
     pid = fork();
-    if(pid < 0) return printf("Child could not be created.\n");
+    if(pid < 0) 
+    {
+        printf("Child could not be created.\n");
+        return;
+    }    
     else if(pid == 0)    // child code
     {
         if( execvp(args[0], args) == -1)
@@ -28,14 +58,24 @@ void execute(char** args)
     }
 }
 
+void execute(char** args)
+{
+    if(strcmp(args[0], "exit") == 0) exit(0);
+    else if(strcmp(args[0], "clear") == 0) system("clear");
+    else if(strcmp(args[0], "cd") == 0)  cd(args);
+    else if(strcmp(args[0], "pwd") == 0) printf("%s\n", getcwd(currentDirectory, 1024));
+    else launch(args);
+    
+}
+
 int main(int argc, char* argv[])
 {
     char line[MAXLEN];
     char* args[MAXARGS];
 
-    while(1)
+    while(true)
     {
-        printf("@> ");
+        prompt();
         fgets(line, MAXLEN, stdin);     // read line from user
         if( (args[0] = strtok(line, " \n\t")) == NULL) continue;
         int i = 1;

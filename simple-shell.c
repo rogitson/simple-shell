94fd61474410a3ae2c0a8c42aa65c8451e5d1249
CulturedMan <77681600+CulturedMan@users.noreply.github.com>
2022-05-24 18:26:34 +0200

    Update simple-shell.c
    - Fixed exit bug
    - Added Clear

--- simple-shell.c
+++ simple-shell.c
@@ -12,20 +12,35 @@ void execute(char** args)
     if(strcmp(args[0], "exit") == 0) exit(0);
     pid_t pid;
     pid = fork();
-    if(pid < 0) return printf("Child could not be created.\n");
+    if(pid < 0) return;
     else if(pid == 0)    // child code
-    {
-        if( execvp(args[0], args) == -1)
+    {   if(execvp(args[0], args) == -1) 
         {
-            printf("%s: command not found\n", args[0]);
+            printf("%s: command not found\n",args[0]);  
             fflush(stdout);
-            kill(getpid(), SIGTERM);
+            kill(getpid(),SIGTERM);
         }
     }
     else                // parent code
     {
         waitpid(pid, NULL, 0);
     }
+}
+
+void printargs(char** args)
+{
+    int i = 0;
+    while(args[i] != NULL)
+    {
+        printf("%s\n", args[i++]);
+    }
+}
+
+void shellPrompt(){
+    // We print the prompt in the form "<user>@<host> <cwd> >"
+    char hostn[1204] = "";
+    gethostname(hostn, sizeof(hostn));
+    printf("%s@%s %s > ", getenv("LOGNAME"), hostn, getcwd((char*) calloc(1024, sizeof(char)), 1024));
 }
 
 int main(int argc, char* argv[])
@@ -35,11 +50,15 @@ int main(int argc, char* argv[])
 
     while(1)
     {
-        printf("@> ");
+        // printf("@> ");
+        shellPrompt();
         fgets(line, MAXLEN, stdin);     // read line from user
-        if( (args[0] = strtok(line, " \n\t")) == NULL) continue;
+        // printf("line is %s\n", line);
+        args[0] = strtok(line, " \n\t");
+        // printf("arg is %s\n", args[0]);
         int i = 1;
         while( (args[i] = strtok(NULL, " \n\t")) != NULL) ++i;
+        // printargs(args);
         execute(args);
     }
 }
